d51137ed76fcade9d6ade32e7f7c7e6827fc36b4
Vladimir Kriukov <zeoniss28@gmail.com>
2021-01-31 17:20:19 +0200

    summary

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './styles.css';
+
+// Templating
+
+import menu from './menu.json';
+
+import template from './gallery-items.hbs';
+
+const markup = template(menu);
+
+const container = document.querySelector('.js-menu');
+
+container.innerHTML = markup;
+
+// Theme-adding
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+const existingTheme = localStorage.getItem('Theme');
+const parsedExistingTheme = JSON.parse(existingTheme);
+
+if (existingTheme) {
+    document.body.classList.add(parsedExistingTheme);
+}
+
+const refs = {
+    switcher: document.querySelector('#theme-switch-toggle'),
+};
+
+if (existingTheme === JSON.stringify(Theme.DARK)) {
+    refs.switcher.checked = true;
+}
+
+refs.switcher.addEventListener('change', changeTheme);
+
+function changeTheme(event) {
+    if (event.target.checked) {
+        document.body.classList.remove(existingTheme);
+        document.body.classList.add('dark-theme');
+        localStorage.setItem('Theme', JSON.stringify(Theme.DARK));
+    } else {
+        document.body.classList.remove(existingTheme);
+        document.body.classList.remove('dark-theme');
+        localStorage.setItem('Theme', JSON.stringify(Theme.LIGHT));
+    }
+}
